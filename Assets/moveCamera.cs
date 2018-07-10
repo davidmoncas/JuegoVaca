fa2b840fb4e0401465e5feee82cf7b5d3da0813b
David Montoya <30814399+davidmoncas@users.noreply.github.com>
2018-07-09 21:48:35 -0500

    Add files via upload

---
+++ Assets/moveCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour {
+
+	public Transform Player;
+	public Vector3 CameraDistance;
+	public GameObject[] pisos= new GameObject[3];
+
+	private Vector3 moveTo;
+
+	// Use this for initialization
+	void Start () {
+		moveTo=this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i=0;i<pisos.Length;i++)
+		{
+			if (pisos[i].GetComponent<MeshCollider>().bounds.Contains(Player.position))
+			{
+				
+				moveTo=pisos[i].transform.position+CameraDistance;
+			}
+		}
+
+		this.transform.position=Vector3.Lerp(this.transform.position,moveTo,0.1f);
+	}
+}
---
+++ Assets/moverTouch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverTouch : MonoBehaviour {
+ 
+ 
+ private Vector3 pos;
+ private Vector3 goTo;
+
+
+ void Start()
+ {
+	 
+	 goTo=this.transform.position;
+ }
+
+ void Update()
+ {
+
+	 if (Input.GetMouseButton(0)) 
+	 {
+		
+		goTo=GetWorldPositionOnPlane(Input.mousePosition , 0f);
+		
+	 }
+	
+	
+	if ( Mathf.Abs( Vector3.Distance(goTo,this.transform.position) ) > 0.1f )
+	{
+		this.transform.position+=(goTo-this.transform.position).normalized*Time.deltaTime*10;
+	}
+
+ }
+ public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float y) {
+     Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+     Plane xy = new Plane(Vector3.up, new Vector3(0, y, 0));
+     float distance;
+     xy.Raycast(ray, out distance);
+     return ray.GetPoint(distance);
+ }
+
+}
